725ddf484864716364bf137ea8c31492616da284
31sayyedsoleha-bit <31sayyedsoleha@gmail.com>
2025-11-10 20:56:09 +0530

    Add factorial program using iteration and recursion in C

---
+++ factorial.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+// Function to find factorial using iteration
+long long factorial_iterative(int n) {
+    long long fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+// Function to find factorial using recursion
+long long factorial_recursive(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial_recursive(n - 1);
+}
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0)
+        printf("Factorial is not defined for negative numbers.\n");
+    else {
+        printf("Factorial of %d using iteration = %lld\n", num, factorial_iterative(num));
+        printf("Factorial of %d using recursion = %lld\n", num, factorial_recursive(num));
+    }
+
+    return 0;
+}
